06ed1bfe3cd672d779bd37b96cd7e19b0209701b
Programming-skylark <67531002+Programming-skylark@users.noreply.github.com>
2020-08-27 23:56:45 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Purchage from './components/Purchage/Purchage';
+import Boot from './components/Boot/Boot';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Purchage></Purchage>
+      <Boot></Boot>
+      
       
     </div>
   );
---
+++ src/components/Boot/Boot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Boot = () => {
+    return (
+        <div className='container' style={{backgroundColor:'lightSteelBlue'}}>
+            <h6>using bootstrap</h6>
+        </div>
+    );
+};
+
+export default Boot;
